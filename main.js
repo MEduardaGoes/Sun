8edf894ad282bec564657a40f5e40d4a1c93d42f
King <79610976+kingzinhor@users.noreply.github.com>
2022-09-28 18:35:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-document.getElementById("");
+var currentPage = 1;
+
+document.getElementById(`pag${currentPage}`).style.display = 'block';
+
+function nextPage(){
+    nextPage = currentPage + 1;
+    updatePage(nextPage);
+}
+
+function prevPage(){
+    nextPage = currentPage - 1;
+    updatePage(nextPage);
+}
+
+function updatePage(pageNumber){
+    document.getElementById(`pag${currentPage}`).style.display = 'none';
+    document.getElementById(`pag${pageNumber}`).style.display = 'block';
+}
